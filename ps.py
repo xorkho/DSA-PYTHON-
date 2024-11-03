a91ef08f2799c9e2547d5bc748b6bf76f0a00523
Xorkho <muhammadumerahmed95@gmail.com>
2024-11-03 17:52:44 +0500

    new

---
+++ ps.py
@@ -0,0 +1,15 @@
+# stack1=[]
+# stack2=[]
+# k=1
+# num="10200"
+# for i in range(len(num)-1,-1,-1):
+#     if i>k:
+#         stack1.append(num[i])
+#     else:
+#         stack2.append(num[i])
+# print(stack1)
+# print(stack2)
+
+l=[3,4]
+l1=[3,4]
+print(l==l1)
